85e3006d554d36187fc264d2a89ca9ce39d137e2
silvestrevivo <silvestrevivomillan@gmail.com>
2018-07-24 23:51:21 +0200

    refractoring store with new functions

--- store/squadstore.js
+++ store/squadstore.js
@@ -1,22 +1,27 @@
 import { observable, action, computed  } from 'mobx'
 import data from '../data/characters.json';
 
+function toFilterArray(obs, id) {
+  return obs.filter(item => item.id !== id);
+}
+
+function addElementArray(obs, id) {
+  return obs.filter(item => item.id === id)[0];
+}
+
 class SquadStore{
   @observable characters = data;
   @observable heroes = [];
 
   @action addCharacterById = (id) => {
-    console.log('addCharacterById')
-    let filterArray = this.characters.filter(item => item.id !== id);
-    let newElement = this.characters.filter(item => item.id === id)[0];
+    let filterArray = toFilterArray(this.characters, id);
+    let newElement = addElementArray(this.characters, id);
     this.characters.replace(filterArray)
     this.heroes.push(newElement)
   }
-
   @action removeCharacterById = (id) => {
-    console.log('removeCharacterById')
-    let array = this.heroes.filter(item => item.id !== id);
-    let element = this.heroes.filter(item => item.id === id)[0];
+    let array = toFilterArray(this.heroes, id);
+    let element = addElementArray(this.heroes, id);
     this.characters.push(element);
     this.heroes.replace(array);
   }
@@ -26,19 +31,16 @@ class SquadStore{
     this.heroes.forEach(hero => strength += hero.strength);
     return strength;
   }
-
   @computed get intelligence(){
       let intelligence = 0;
       this.heroes.forEach(hero => intelligence += hero.intelligence);
       return intelligence;
   }
-
   @computed get speed(){
       let speed = 0;
       this.heroes.forEach(hero => speed += hero.speed);
       return speed;
   }
-
 }
 
 var store = new SquadStore()
